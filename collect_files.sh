09aefe758b2beb81fe327342ee7f54886aeefff4
krutoichelik <98899358+krutoichelik@users.noreply.github.com>
2025-04-27 22:48:45 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-python3 pythonScript.py "$@"
+
+SRC="$1"
+DST="$2"
+DEPTH=""
+
+if [ "$3" = "--max_depth" ]; then
+  DEPTH="$4"
+elif echo "$3" | grep -q "^--max_depth="; then
+  DEPTH=$(echo "$3" | cut -d'=' -f2)
+fi
+
+javac CollectorMain.java
+
+if [ $? -eq 0 ]; then
+  if [ -n "$DEPTH" ]; then
+    java CollectorMain "$SRC" "$DST" "$DEPTH"
+  else
+    java CollectorMain "$SRC" "$DST"
+  fi
+else
+  exit 1
+fi
