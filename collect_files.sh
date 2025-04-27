2139f3efa25ee48c468456b8612b3b4733382252
krutoichelik <98899358+krutoichelik@users.noreply.github.com>
2025-04-27 22:53:14 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -10,14 +10,10 @@ elif echo "$3" | grep -q "^--max_depth="; then
   DEPTH=$(echo "$3" | cut -d'=' -f2)
 fi
 
-javac CollectorMain.java
+javac CollectorMain.java >/dev/null 2>&1
 
-if [ $? -eq 0 ]; then
-  if [ -n "$DEPTH" ]; then
-    java CollectorMain "$SRC" "$DST" "$DEPTH"
-  else
-    java CollectorMain "$SRC" "$DST"
-  fi
+if [ -n "$DEPTH" ]; then
+  java CollectorMain "$SRC" "$DST" "$DEPTH" >/dev/null 2>&1
 else
-  exit 1
+  java CollectorMain "$SRC" "$DST" >/dev/null 2>&1
 fi
